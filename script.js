57d0ad69f4c9bb200b542053272890ba5dfa2616
Okeowo Adegbenga <okeowo_adegbenga@yahoo.com>
2025-07-11 12:44:40 +0100

    error in the line of code,syntax error
    debugging error and fixing it.

--- script.js
+++ script.js
@@ -6,9 +6,3 @@
 document.getElementById('examount').value = calculatedAmount;
 
 }
-
-/*let fruits = ["mango","apple","pineapple","banana","orange"];
-/*item = fruuts.includes("banana");
-/*console.log(fruits.length);
-console.log(fruits[4]);
-console.log(item)
