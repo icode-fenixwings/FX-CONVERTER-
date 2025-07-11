6b8f02d646e551c6562bda74ce92a87e055a08a5
Okeowo Adegbenga <okeowo_adegbenga@yahoo.com>
2025-07-11 10:15:15 +0100

    FX CONVERTER UPLOAD
    Add dollar amount and  convert it to naira at the click of a button.

---
+++ script.js
@@ -0,0 +1,14 @@
+ function fxConverter() {
+ amountE = document.getElementById('amount').value;
+ exRate = 1600;
+ calculatedAmount = amountE * exRate;
+
+document.getElementById('examount').value = calculatedAmount;
+
+}
+
+/*let fruits = ["mango","apple","pineapple","banana","orange"];
+/*item = fruuts.includes("banana");
+/*console.log(fruits.length);
+console.log(fruits[4]);
+console.log(item)
